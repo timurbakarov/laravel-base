9a64dc8804cb3ad9e768046a3ebeeb2d3bd04ee5
Timur Bakarov <tbakarov@gmail.com>
2015-04-10 18:36:08 +0300

    Add readme

---
+++ resources/lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Passwords must be at least six characters and match the confirmation.",
+	"user" => "Пользователь с указанной почтой не найден",
+	"token" => "This password reset token is invalid.",
+	"sent" => "На вашу почту отправлено письмо с новым паролем",
+	"reset" => "Your password has been reset!",
+
+];
---
+++ resources/lang/ru/validation.php
@@ -0,0 +1,107 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Validation Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines contain the default error messages used by
+	| the validator class. Some of these rules have multiple versions such
+	| as the size rules. Feel free to tweak each of these messages here.
+	|
+	*/
+
+	"accepted"             => "The :attribute must be accepted.",
+	"active_url"           => "The :attribute is not a valid URL.",
+	"after"                => "The :attribute must be a date after :date.",
+	"alpha"                => "The :attribute may only contain letters.",
+	"alpha_dash"           => "Поле :attribute может содержать только буквы, цифры и сивол нижнего подчеркивания",
+	"alpha_num"            => "The :attribute may only contain letters and numbers.",
+	"array"                => "The :attribute must be an array.",
+	"before"               => "The :attribute must be a date before :date.",
+	"between"              => [
+		"numeric" => "The :attribute must be between :min and :max.",
+		"file"    => "The :attribute must be between :min and :max kilobytes.",
+		"string"  => "The :attribute must be between :min and :max characters.",
+		"array"   => "The :attribute must have between :min and :max items.",
+	],
+	"boolean"              => "The :attribute field must be true or false.",
+	"confirmed"            => "Пароли не совпадают",
+	"date"                 => "The :attribute is not a valid date.",
+	"date_format"          => "Неверный формат даты",
+	"different"            => "The :attribute and :other must be different.",
+	"digits"               => "The :attribute must be :digits digits.",
+	"digits_between"       => "The :attribute must be between :min and :max digits.",
+	"email"                => "Неверный формат почты",
+	"filled"               => "The :attribute field is required.",
+	"exists"               => "The selected :attribute is invalid.",
+	"image"                => "The :attribute must be an image.",
+	"in"                   => "The selected :attribute is invalid.",
+	"integer"              => "The :attribute must be an integer.",
+	"ip"                   => "The :attribute must be a valid IP address.",
+	"max"                  => [
+		"numeric" => "The :attribute may not be greater than :max.",
+		"file"    => "The :attribute may not be greater than :max kilobytes.",
+		"string"  => "The :attribute may not be greater than :max characters.",
+		"array"   => "The :attribute may not have more than :max items.",
+	],
+	"mimes"                => "Файл должен быть в формате :values",
+	"min"                  => [
+		"numeric" => ":attribute должен быть не меньше :min символов",
+		"file"    => "The :attribute must be at least :min kilobytes.",
+		"string"  => "Поле :attribute должно быть не меньше :min символов",
+		"array"   => "The :attribute must have at least :min items.",
+	],
+	"not_in"               => "The selected :attribute is invalid.",
+	"numeric"              => "The :attribute must be a number.",
+	"regex"                => "The :attribute format is invalid.",
+	"required"             => "Поле обязательно для заполнения",
+	"required_if"          => "The :attribute field is required when :other is :value.",
+	"required_with"        => "Поле обязательно для заполнения",
+	"required_with_all"    => "The :attribute field is required when :values is present.",
+	"required_without"     => "Заполните :attribute, либо :values",
+	"required_without_all" => "The :attribute field is required when none of :values are present.",
+	"same"                 => "The :attribute and :other must match.",
+	"size"                 => [
+		"numeric" => "The :attribute must be :size.",
+		"file"    => "The :attribute must be :size kilobytes.",
+		"string"  => "The :attribute must be :size characters.",
+		"array"   => "The :attribute must contain :size items.",
+	],
+	"unique"               => ":attribute уже используется",
+	"url"                  => "Неправильный формат",
+	"timezone"             => "The :attribute must be a valid zone.",
+
+	/*
+	|--------------------------------------------------------------------------
+	| Custom Validation Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| Here you may specify custom validation messages for attributes using the
+	| convention "attribute.rule" to name the lines. This makes it quick to
+	| specify a specific custom language line for a given attribute rule.
+	|
+	*/
+
+	'custom' => [
+		'attribute-name' => [
+			'rule-name' => 'custom-message',
+		]
+	],
+
+	/*
+	|--------------------------------------------------------------------------
+	| Custom Validation Attributes
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used to swap attribute place-holders
+	| with something more reader friendly such as E-Mail Address instead
+	| of "email". This simply helps us make messages a little cleaner.
+	|
+	*/
+
+	'attributes' => [],
+
+];
